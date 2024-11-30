e26def507252aa0104e11c75fdf214cf3a5ca4a7
Mohammad Banisaeid <smbl64@gmail.com>
2024-11-30 13:24:20 +0100

    Implement the scrape method using Jina Reader API

--- cmd/tools/scrape.go
+++ cmd/tools/scrape.go
@@ -1,11 +1,56 @@
 package tools
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
 
 var scrapeCmd = &cobra.Command{
-	Use:   "scrape",
+	Use:   "scrape <url>",
 	Short: "Scrape a website and print the text",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return errors.New("url is not specified")
+		}
+
+		fullURL := fmt.Sprintf("https://r.jina.ai/%s", args[0])
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute*1)
+		defer cancel()
+
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, fullURL, nil)
+		if err != nil {
+			cmd.PrintErrf("failed to create request: %v", err)
+			return nil
+		}
+
+		client := http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			cmd.PrintErrf("call failed: %v", err)
+			return nil
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			cmd.PrintErrf("Call failed with status code %v", resp.Status)
+			return nil
+		}
+
+		bb, err := io.ReadAll(resp.Body)
+		if err != nil {
+			cmd.PrintErrf("Failed to read response body: %v", err)
+		}
+
+		fmt.Println(string(bb))
+
 		return nil
 	},
 }
