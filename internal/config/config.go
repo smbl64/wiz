9171c75f6a95ed7bac749c542a96e709240e2493
Mohammad Banisaeid <smbl64@gmail.com>
2024-12-02 21:17:31 +0100

    Read patterns from program's own config folder

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,46 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
 type Config struct {
 	PatternsDir   string
 	Model         string
 	OllamaAPIBase string
 }
 
-func Current() *Config {
+var configDir string
+
+// Init creates the config folder if it doesn't already exist.
+func Init() error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("cannot detect home directory: %v", err)
+	}
+
+	configDir = path.Join(home, ".config", "wiz")
+	err = os.MkdirAll(configDir, 0700)
+	if err != nil {
+		return fmt.Errorf("cannot create config directory: %v", err)
+	}
+
+	return nil
+}
+
+func defaultConfig() *Config {
+
 	return &Config{
-		PatternsDir:   "/Users/mohammad/.config/fabric/patterns",
+		PatternsDir:   path.Join(configDir, "patterns"),
 		Model:         "qwen2.5:latest",
 		OllamaAPIBase: "http://localhost:11434",
 	}
+}
+
+func Current() *Config {
+	return defaultConfig()
 }
 
 // Load the default configuration.
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/smbl64/wiz/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/smbl64/wiz/cmd"
+	"github.com/smbl64/wiz/internal/config"
+)
 
 func main() {
+	if err := config.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
 	cmd.Execute()
 }
